79b713ae02c2c07a5a068b5ac4cbe0f2d1d24d50
Ahmed sultan <en.ahmedsultan@gmail.com>
2020-10-03 17:44:44 +0200

    feat init project files structure

---
+++ CovidHub/Countries List Screen/CountryListViewController.swift
@@ -0,0 +1,12 @@
+//
+//  CountryListViewController.swift
+//  CovidHub
+//
+//  Created by Ahmed Sultan on 10/3/20.
+//
+
+import UIKit
+
+class CountryListViewController: UITableViewController {
+    
+}
---
+++ CovidHub/Countries List Screen/CountryListViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  CountryListViewModel.swift
+//  CovidHub
+//
+//  Created by Ahmed Sultan on 10/3/20.
+//
+
+import Foundation
+struct CountryListViewModel {
+    
+}
